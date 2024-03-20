d68e035ef53baaa7a97e1e977f67ef6951819774
jormick <65humuge@gmail.com>
2024-03-20 23:57:23 +0300

    js code for todo list workin

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("test")
+let inputField = document.querySelector('.input-field'),
+    addButton = document.querySelector('.add'),
+    userPlans = document.querySelector('.user-plans');
+
+    let planSet = [];
+
+    addButton.addEventListener('click', function(){
+        
+        let newPlan = {
+            planText: inputField.value,
+            checked: false
+        };
+
+        planSet.push(newPlan);
+        showPlan();
+    });
+
+    function showPlan(){
+        let showPlan = '';
+        planSet.forEach(function(item, i){
+            showPlan += `
+            <li>
+                <input type='checkbox' id='item_${i}'
+                <label for='item_${i}'>${item.planText}</label>
+            </li>
+            `;
+            userPlans.innerHTML = showPlan;
+        });
+
+    }
