9cf777d4e9a50aa3268dd55d735b52fdb631686a
Vlad Kozlov <65humuge@gmail.com>
2024-04-23 00:27:17 +0300

    new plans can be added

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 let inputField = document.querySelector('.input-field'),
     addButton = document.querySelector('.add'),
-    userPlans = document.querySelector('.user-plans');
+    userPlans = document.querySelector('.user-plans'),
+    deleteButton = document.querySelector('.delete-button');
 
-    let planSet = [];
+let planSet = [];
 
-    addButton.addEventListener('click', function(){
-        
-        let newPlan = {
-            planText: inputField.value,
-            checked: false
-        };
+if (localStorage.getItem('todo')) {
+    planSet = JSON.parse(localStorage.getItem('todo'));
+    toDoListDisplay();
+}
 
-        planSet.push(newPlan);
-        showPlan();
-    });
+addButton.addEventListener('click', function () {
 
-    function showPlan(){
-        let showPlan = '';
-        planSet.forEach(function(item, i){
-            showPlan += `
+    let newPlan = {
+        planText: inputField.value,
+        checked: false
+    };
+
+    planSet.push(newPlan);
+    toDoListDisplay();
+    localStorage.setItem('todo', JSON.stringify(planSet));
+});
+
+function toDoListDisplay() {
+    let showPlan = '';
+    planSet.forEach(function (item, i) {
+        showPlan += `
             <li>
-                <input type='checkbox' id='item_${i}'
-                <label for='item_${i}'>${item.planText}</label>
+                <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+                <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.planText}</label>
+                <button class="delete-button" id='item_${i}'>X</button>
             </li>
             `;
-            userPlans.innerHTML = showPlan;
-        });
+        userPlans.innerHTML = showPlan;
+    });
+};
 
-    }
+userPlans.addEventListener('change', function (event) {
+    let valueLabel = userPlans.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
+    planSet.forEach(function (item) {
+        if (item.planText === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(planSet));
+        }
+    });
+});
