6f4533a03b2191db578282214df00e9d51a9bb48
Vlad Kozlov <65humuge@gmail.com>
2024-06-04 19:49:28 +0300

    new plans can be added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let inputField = document.querySelector('.input-field'),
     addButton = document.querySelector('.add'),
-    userPlans = document.querySelector('.user-plans'),
-    deleteButton = document.querySelector('.delete-button');
+    userPlans = document.querySelector('.plans-container'),
+    actualPlan = document.querySelector('.actual-plan');
 
 let planSet = [];
 
@@ -12,28 +12,35 @@ if (localStorage.getItem('todo')) {
 
 addButton.addEventListener('click', function () {
 
-    let newPlan = {
-        planText: inputField.value,
-        checked: false
-    };
+    if (inputField.value !== '') {
+        let newPlan = {
+            planText: inputField.value,
+            checked: false
+        };
 
-    planSet.push(newPlan);
-    toDoListDisplay();
-    localStorage.setItem('todo', JSON.stringify(planSet));
+        planSet.push(newPlan);
+        toDoListDisplay();
+        localStorage.setItem('todo', JSON.stringify(planSet));
+    } else {
+        alert("Please, write your plan")
+    }
+    inputField.value = '';
 });
 
 function toDoListDisplay() {
     let showPlan = '';
     planSet.forEach(function (item, i) {
         showPlan += `
-            <li>
-                <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-                <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.planText}</label>
-                <button class="delete-button" id='item_${i}'>X</button>
+            <li class="actual-plan">
+                <div class="plan-text">
+                    <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+                    <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.planText}</label>
+                </div>
+                <button class="delete-button" id='item_${i}'>Del</button>
             </li>
             `;
-        userPlans.innerHTML = showPlan;
     });
+    userPlans.innerHTML = showPlan;
 };
 
 userPlans.addEventListener('change', function (event) {
@@ -44,4 +51,26 @@ userPlans.addEventListener('change', function (event) {
             localStorage.setItem('todo', JSON.stringify(planSet));
         }
     });
+});
+
+userPlans.addEventListener('click', function (event) {
+    if (event.target.classList.contains('delete-button')) {
+        let index = event.target.getAttribute('data-index');
+        planSet.splice(index, 1);
+        toDoListDisplay();
+        localStorage.setItem('todo', JSON.stringify(planSet));
+    }
+});
+
+userPlans.addEventListener('click', function (event) {
+    if (event.target.classList.contains()) {
+        let index = event.target.getAttribute('data-index');
+        planSet.splice(index, 1);
+        toDoListDisplay();
+        localStorage.setItem('todo', JSON.stringify(planSet));
+    }
+});
+
+userPlans.addEventListener('click', function(event) {
+
 });
