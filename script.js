f98a768700a4e859e0fc442a2073f6e439b22a7b
Vlad Kozlov <65humuge@gmail.com>
2024-06-06 15:57:40 +0300

    Now, plans can be added, deleted and changed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let inputField = document.querySelector('.input-field'),
     addButton = document.querySelector('.add'),
-    userPlans = document.querySelector('.plans-container'),
-    actualPlan = document.querySelector('.actual-plan');
+    userPlans = document.querySelector('.plans-container');
 
 let planSet = [];
 
@@ -10,8 +9,18 @@ if (localStorage.getItem('todo')) {
     toDoListDisplay();
 }
 
+// Using the mouse and the “Enter” key to add a plan
 addButton.addEventListener('click', function () {
+    addPlan();
+});
 
+inputField.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        addPlan();
+    }
+});
+
+function addPlan() {
     if (inputField.value !== '') {
         let newPlan = {
             planText: inputField.value,
@@ -25,24 +34,57 @@ addButton.addEventListener('click', function () {
         alert("Please, write your plan")
     }
     inputField.value = '';
-});
+};
 
+// Add new plan
 function toDoListDisplay() {
     let showPlan = '';
     planSet.forEach(function (item, i) {
         showPlan += `
-            <li class="actual-plan">
+           <li class="actual-plan">
                 <div class="plan-text">
                     <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-                    <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.planText}</label>
+                    <div class="todo-item-text" for='item_${i}' id="${item.important ? 'important' : ''}">${item.planText}</div>
                 </div>
                 <button class="delete-button" id='item_${i}'>Del</button>
             </li>
             `;
     });
     userPlans.innerHTML = showPlan;
-};
 
+    document.querySelectorAll('.todo-item-text').forEach(function (planTextChange) {
+        planTextChange.addEventListener('click', editPlan);
+    });
+}
+
+function editPlan(event) {
+    let planTextChange = event.target;
+    let currentText = planTextChange.innerText;
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.value = currentText;
+    input.classList.add('edit-input');
+    planTextChange.replaceWith(input);
+    input.focus();
+
+    function saveEdit() {
+        let newText = input.value;
+        planTextChange.innerText = newText;
+        input.replaceWith(planTextChange);
+        let index = planTextChange.parentElement.parentElement.getAttribute('data-index');
+        planSet[index].planText = newText;
+        localStorage.setItem('todo', JSON.stringify(planSet));
+    }
+
+    input.addEventListener('blur', saveEdit);
+    input.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            input.blur();
+        }
+    });
+}
+
+// Changing checkbox status and saving it in local storage
 userPlans.addEventListener('change', function (event) {
     let valueLabel = userPlans.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
     planSet.forEach(function (item) {
@@ -53,6 +95,7 @@ userPlans.addEventListener('change', function (event) {
     });
 });
 
+// Using "Del" button for delete current plan
 userPlans.addEventListener('click', function (event) {
     if (event.target.classList.contains('delete-button')) {
         let index = event.target.getAttribute('data-index');
@@ -62,15 +105,3 @@ userPlans.addEventListener('click', function (event) {
     }
 });
 
-userPlans.addEventListener('click', function (event) {
-    if (event.target.classList.contains()) {
-        let index = event.target.getAttribute('data-index');
-        planSet.splice(index, 1);
-        toDoListDisplay();
-        localStorage.setItem('todo', JSON.stringify(planSet));
-    }
-});
-
-userPlans.addEventListener('click', function(event) {
-
-});
